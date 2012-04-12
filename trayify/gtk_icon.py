d9540143da3467b6196294ba22efdbb60e61e016
Kristofer M White <me@kmwhite.net>
2012-04-12 16:25:00 -0500

    Adding imports for GTK imports

--- trayify/gtk_icon.py
+++ trayify/gtk_icon.py
@@ -1,3 +1,19 @@
+# Standard Library Imports
+from __future__ import print_function
+import sys
+
+# Third Party imports
+try:
+    import gtk
+    import gobject
+except ImportError:
+    print('Import Failure: GTK Not Availible', file=sys.stderr)
+    print('Ensure gtk, and gobject are available', file=sys.stderr)
+    sys.exit(2)
+
+# Local Imports
+# none
+
 class NotificationIcon(object):
     ''' The Gtk Icon Instance '''
 
