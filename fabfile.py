2d75fed534e20cdc45695de04bcf8fc70e581e04
Kristofer M White <me@kmwhite.net>
2012-04-12 01:26:29 -0500

    Adding rudimentary fabfile for testing

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+
+from fabric.api import env, local
+import os
+
+env.hosts = ['localhost']
+#env.user = 'deploy'
+#env.keyfile = ['$HOME/.ssh/deploy_rsa']
+#env.directory = '/path/to/virtualenvs/project'
+#env.activate = 'source /path/to/virtualenvs/project/bin/activate'
+
+def test():
+    local('py.test trayify test_trayify.py')
+
+def coverage():
+    local('py.test --cov trayify test_trayify.py')
