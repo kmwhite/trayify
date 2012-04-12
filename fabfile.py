ca576d3c6e733bb7e3931cafe65f5424846dcaca
Kristofer M White <me@kmwhite.net>
2012-04-12 02:10:20 -0500

    Adding docstrings and clean() to fab file

--- fabfile.py
+++ fabfile.py
@@ -9,15 +9,23 @@ env.hosts = ['localhost']
 #env.activate = 'source /path/to/virtualenvs/project/bin/activate'
 
 def test():
+    ''' Run the unit tests '''
     local('py.test trayify test_trayify.py')
 
 def coverage():
+    ''' Run the unit tests and generate a coverage report '''
     local('py.test --cov trayify test_trayify.py')
 
 def pep8():
+    ''' Check the code base for PEP8 Compatibility '''
     local('pep8 .')
 
 def coverage_loop():
+    ''' Run the coverage report in an infinite loop '''
     while True:
         coverage()
         local('read')
+
+def clean():
+    ''' Clean the *.pyc files from the working tree '''
+    local('find . -iname "*.pyc" -exec rm -v {} \;')
