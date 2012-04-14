7ecc639dbfee5614fb6af531137011e251d79441
Kristofer M White <me@kmwhite.net>
2012-04-13 19:11:19 -0500

    Removing ImportError catch as it should fail regardless

--- trayify/gtk_icon.py
+++ trayify/gtk_icon.py
@@ -3,13 +3,8 @@ from __future__ import print_function
 import sys
 
 # Third Party imports
-try:
-    import gtk
-    import gobject
-except ImportError:
-    print('Import Failure: GTK Not Availible', file=sys.stderr)
-    print('Ensure gtk, and gobject are available', file=sys.stderr)
-    sys.exit(2)
+import gtk
+import gobject
 
 # Local Imports
 # none
