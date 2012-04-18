bc20178fe2205f01ffe381c77d22327899209376
Kristofer M White <me@kmwhite.net>
2012-04-17 23:11:57 -0500

    Adding initial setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+setup(
+    name = "trayify",
+    version = "0.0.1",
+    packages = find_packages(),
+    scripts = [],
+
+    # Purposely left empty. This could contain PyQT or PyGTK, but we're not
+    # enforcing that a package be installed on the machine before we can be
+    # used. If someone only wants GTK apps, they shouldn't be required to have
+    # the PyQT libraries installed as well.
+    install_requires = [],
+
+    package_data = {
+        'trayify': ['*.py'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Kristofer M White",
+    author_email = "me@kmwhite.net",
+    description = "A python module for super simple system tray applications",
+    license = "BSD",
+    keywords = "system tray gtk qt",
+    url = "http://kmwhite.github.com/trayify/",
+)
